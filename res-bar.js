5ff8c06e831f8e43627e9804ecbbac636b5b73de
andydick1947 <ardick2000@hotmail.com>
2024-09-20 06:19:28 +0100

    add result

--- res-bar.js
+++ res-bar.js
@@ -21,6 +21,7 @@ let myResults = [
     ["22 AUG 24","Ranks 8 22 24.htm"],
     ["29 AUG 24","Ranks 8 29 24.htm"],
     ["5 SEP 24","Ranks 9 5 24.htm"],
-    ["12 SEP 24","Ranks 9 12 24.htm"]   
+    ["12 SEP 24","Ranks 9 12 24.htm"],
+    ["19 SEP 24","Ranks 9 19 24.htm"]    
     ]
 
