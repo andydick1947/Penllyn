4f241a5f7b214d39a6d8ffa611955a40e2385d2f
andydick1947 <ardick2000@hotmail.com>
2025-08-27 21:28:57 +0100

    add update

--- index.js
+++ index.js
@@ -300,3 +300,16 @@ for (let x=1; x<=9; x++){
  
    })
    }
+
+  window.addEventListener("load",function(){
+      setTimeout(
+         function open(event){
+      document.querySelector(".popup").style.display="block";
+   }),
+   1000
+   });
+
+
+   document.querySelector("#close").addEventListener("click",function(){
+      document.querySelector(".popup").style.display="none";
+   });
